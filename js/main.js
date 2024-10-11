ddc8bfa00e68dc7a3b6fa075edece5dd81c5e183
Justin Fanton <justin.fanton@gmail.com>
2024-10-11 13:07:50 +0200

    implemented obstacles

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ class Player {
   constructor() {
     this.width = 20;
     this.height = 10;
-    this.positionX = 0;
+    this.positionX = 50 - this.width / 2;
     this.positionY = 0;
     this.domElement = null;
 
@@ -32,7 +32,51 @@ class Player {
   }
 }
 
+
+class Obstacle {
+    constructor() {
+    this.width = 30;
+    this.height = 5;
+    this.positionX = 50 - this.width / 2;
+    this.positionY = 100;
+    this.domElement = null;
+
+    this.createDomElement();
+    }
+
+    createDomElement() {
+        this.domElement = document.createElement("div");
+        
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+    
+        const board = document.getElementById("board");
+        board.appendChild(this.domElement);
+      }
+
+    moveDown(){
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+}
+
 const player = new Player();
+
+const obstacleArr = [];
+
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstacleArr.push(newObstacle);
+}, 4000)
+
+setInterval(() => {
+    obstacleArr.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown();
+    });
+}, 100)
 
 document.addEventListener('keydown', (e) => {
     if(e.code === 'ArrowLeft'){
